d27a10a01cf70fb109560c3c266980e7851c7e9f
lmaydana <lmaydana@fi.uba.ar>
2025-06-21 17:11:56 -0300

    Agregando datos de estado del jugador al snapshot, la cantidad de veces que murio y el estado del juego bomba explotada

---
+++ server/Deactivator.h
@@ -0,0 +1,8 @@
+//
+// Created by lionel on 21/06/25.
+//
+
+#ifndef DEACTIVATOR_H
+#define DEACTIVATOR_H
+
+#endif //DEACTIVATOR_H
---
+++ server/PlayerStatus.h
@@ -0,0 +1,8 @@
+//
+// Created by lionel on 21/06/25.
+//
+
+#ifndef PLAYERSTATUS_H
+#define PLAYERSTATUS_H
+
+#endif //PLAYERSTATUS_H
